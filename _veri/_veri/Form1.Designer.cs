17fdbc9897769f17a5423030af22ff2eb636a77b
Özlem ÖZKAYA <58657181+ozlemozk@users.noreply.github.com>
2020-06-13 21:17:35 +0300

    Add files via upload

---
+++ _veri/_veri/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace _veri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(716, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 32);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(55, 185);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(458, 360);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(633, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(633, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 70);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "DURDUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(633, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 70);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "KAYDET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(312, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "DEĞERLER\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(573, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PORT İSMİ\r\n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_veri.Properties.Resources.Adsız;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 129);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1080, 604);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ _veri/_veri/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace _veri
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _port;  // Seri port nesnesi oluşturuyoruz
+
+        private DateTime datetime;
+        private string in_data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _port = new SerialPort();
+            _port.BaudRate = 9600; //Kullanacağımız seri port hızını 
+            _port.PortName = textBox1.Text; //Kullanacağımız seri port adını
+            _port.Parity = Parity.None; //  Gönderilen verinin doğruluğunu kontrol etmek 
+            _port.DataBits = 8; //göndereceğimiz bilginin kaç bitten olduğu
+            _port.StopBits = StopBits.One; //Stop bitinin kaç bit olacağını belirtir.
+            _port.DataReceived += my_DataTecevied;
+            try
+            {
+                _port.Open();
+              textBox2.Text = "";
+               // listBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hata Oluştu");
+
+            }
+        }
+        void my_DataTecevied(object sender, SerialDataReceivedEventArgs e)
+        {
+            in_data = _port.ReadLine();
+            //Veriler alınır
+
+            this.Invoke(new EventHandler(displaydata_event));
+
+        }
+        private void displaydata_event(object sender, EventArgs e)
+        {
+            //Gelener verileri güncel zaman ile ekle
+            datetime = DateTime.Now;
+            string time = datetime.Hour + ":" + datetime.Minute + ":" + datetime.Second;
+   
+            textBox2.AppendText(time + "\t\t\t" + in_data + "\n");
+         
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string pathfile = @"C:\Users\USER\Desktop";
+                string filename = "veri.txt";
+                System.IO.File.WriteAllText(pathfile + filename, textBox2.Text);
+                MessageBox.Show("Kayıt Edildi " + pathfile, "Bilgi");
+            }
+            catch (Exception ex3)
+            {
+
+                MessageBox.Show(ex3.Message, "Hata Oluştu");
+            }
+        }
+
+
+       private void button2_Click(object sender, EventArgs e)
+        {
+            _port.Close(); //seri portu kapat
+
+        }
+    }
+}
